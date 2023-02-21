2e7455ec4b60dc6d95c0d1ea93426716b3f2a785
kalpitsahu22 <62590912+kalpitsahu22@users.noreply.github.com>
2023-02-21 13:27:09 +0530

    Add files via upload
    Eat it up React application

---
+++ 01-starting-project/src/components/Cart/Cart.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+import { useContext } from "react";
+import CartItem from "./CartItem";
+import CartContext from "../../store/cart-context";
+
+const Cart = (props) => {
+  const cartCTX = useContext(CartContext);
+  const totalAmount = `$${cartCTX.totalAmount.toFixed(2)}`;
+  const hasItems = cartCTX.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCTX.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {cartCTX.addItem(item)};
+
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {cartCTX.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null,item)}
+        />
+      ))}
+    </ul>
+  );
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["buttons--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
